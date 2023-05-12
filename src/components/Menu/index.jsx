1b4ad16dce5348b46ffb9717a115e370cd2d434a
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-11 21:27:25 -0300

    🩹fix(componente): Corrige o parametro link para to

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,8 +4,8 @@ export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} link="/">Início</Link>
-                <Link className={styles.link} link="/sobremim">Sobre Mim</Link>
+                <Link className={styles.link} to="/">Início</Link>
+                <Link className={styles.link} to="/sobremim">Sobre Mim</Link>
             </nav>
         </header>
     )
