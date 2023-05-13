d6c82b3f45bf83ce946ea8bf14a2b76df543adcb
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-12 23:52:58 -0300

    🌟feat(implements): Usa o componente Posts importando o JSon

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Banner from 'components/Banner'
+import postsJson from 'json/posts.json'
+import Post from 'components/Post'
+import styles from './Inicio.module.css'
 
 export default function Inicio() {
   return (
     <main>
       <Banner />
-      <h1>Início</h1>
+      <ul className={styles.posts}>
+        {
+          postsJson.map(
+            (post) => {
+              return (
+                <Post post={post} key={post.id}/>
+              )
+            }
+          )
+        }
+      </ul>
     </main>
   )
 }
