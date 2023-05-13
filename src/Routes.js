9f094232eb68e063e06c0f7e363a08b4ce9ecec9
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-13 17:25:34 -0300

    🔄️refactor(router) Utitlizando rota layout padrão + index

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,17 @@ import Inicio from "./paginas/Inicio"
 import SobreMim from "./paginas/SobreMim"
 import Menu from "./components/Menu"
 import Rodape from "components/Rodape"
+import PaginaPadrao from "components/PaginaPadrao"
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />} >
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
         <Route path="*" element={<div>Rota não encontrada</div>} />
       </Routes>
       <Rodape />
