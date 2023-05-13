9ce284ddd28cc3e2909cab1786c0a8a41ef95110
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-13 17:24:51 -0300

    🔄️refactor(pagina) Reestruturando a página Inicio

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import Banner from 'components/Banner'
 import postsJson from 'json/posts.json'
 import Post from 'components/Post'
 import styles from './Inicio.module.css'
 
 export default function Inicio() {
   return (
-    <main>
-      <Banner />
-      <ul className={styles.posts}>
-        {
-          postsJson.map(
-            (post) => {
-              return (
-                <Post post={post} key={post.id}/>
-              )
-            }
-          )
-        }
-      </ul>
-    </main>
+    <ul className={styles.posts}>
+      {
+        postsJson.map(
+          (post) => {
+            return (
+              <Post post={post} key={post.id} />
+            )
+          }
+        )
+      }
+    </ul>
   )
 }
