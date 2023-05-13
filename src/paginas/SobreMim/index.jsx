d7a9cf155f5653e8708d6079c40d085b9beeb500
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-13 17:24:03 -0300

    🔄️refactor(component) Refeito a página SobreMim

--- src/paginas/SobreMim/index.jsx
+++ src/paginas/SobreMim/index.jsx
@@ -1,11 +1,31 @@
-import Banner from 'components/Banner'
-import React from 'react'
+import PostModelo from 'components/PostModelo'
+import styles from './SobreMim.module.css'
+import fotoCapa from 'assets/sobre_mim_capa.png'
+import fotoSobreMim from 'assets/sobre_mim_foto.png'
 
 export default function SobreMim() {
   return (
-    <main>
-      <Banner />
-      <h1>Sobre Mim</h1>
-    </main>
+    <PostModelo
+      fotoCapa={fotoCapa}
+      titulo="Sobre Mim"
+    >
+      <h3 className={styles.subtitulo}>
+        Olá, eu sou o Mauro!
+      </h3>
+
+      <img
+        src={fotoSobreMim}
+        alt="Foto sobre mim"
+        className={styles.fotoSobreMim}
+      />
+
+      <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio voluptas, harum, quidem aspernatur deleniti ducimus laborum ratione aperiam nesciunt porro ullam et accusamus ea error earum corporis quos doloribus!</p>
+      <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio voluptas, harum, quidem aspernatur deleniti ducimus laborum ratione aperiam nesciunt porro ullam et accusamus ea error earum corporis quos doloribus!</p>
+      <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio voluptas, harum, quidem aspernatur deleniti ducimus laborum ratione aperiam nesciunt porro ullam et accusamus ea error earum corporis quos doloribus!</p>
+      <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio voluptas, harum, quidem aspernatur deleniti ducimus laborum ratione aperiam nesciunt porro ullam et accusamus ea error earum corporis quos doloribus!</p>
+      <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio voluptas, harum, quidem aspernatur deleniti ducimus laborum ratione aperiam nesciunt porro ullam et accusamus ea error earum corporis quos doloribus!</p>
+      <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio voluptas, harum, quidem aspernatur deleniti ducimus laborum ratione aperiam nesciunt porro ullam et accusamus ea error earum corporis quos doloribus!</p>
+
+    </PostModelo>
   )
 }
