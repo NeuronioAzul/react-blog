39cae688460a757db9cf5187b28d9aa320a13069
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-16 00:27:51 -0300

    🚚feat(component): Adiciona componente PostModule

--- src/paginas/Post/PostModule/index.jsx
+++ src/paginas/Post/PostModule/index.jsx
@@ -1,18 +1,22 @@
+import PostCard from 'components/PostCard'
 import styles from './PostModule.module.css'
 
-export default function PostModule() {
+export default function PostModule({ posts }) {
     return (
         <div className={styles.postsRecomendados}>
-            <h2>Outros posts que você pode gostar:</h2>
-            {
-                postsRecomendados.map(
-                    (post) => {
-                        return (
-                            <PostCard key={post.id} post={post} />
-                        )
-                    }
-                )
-            }
+            <h2 className={styles.tituloOutrosPosts}>Outros posts que você pode gostar:</h2>
+            <ul className={styles.posts}>
+                {
+                    posts.map(
+                        (post) => {
+                            return (
+                                <PostCard key={post.id} post={post} />
+                            )
+                        }
+                    )
+                }
+            </ul>
+
         </div>
     )
 }
